7ddd7a9ace77387e5bb0b9ba993b425ea2a93add
codeWithVishwa <iamvishwasundar00@gmail.com>
2025-09-21 21:28:13 +0530

    Changes img

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -6,7 +6,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import img1 from "../assets/img/1.jpg";
 import img2 from "../assets/img/2.jpg";
-import img3 from "../assets/img/3.jpg";
+import img3 from "../assets/img/3.JPG";
 import img4 from "../assets/img/4.jpg";
 import img5 from "../assets/img/5.jpg";
 
